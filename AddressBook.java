229b15a08a04b3f6e358e20c4d101fe2536499c3
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-18 00:00:54 +0530

    First Commit

---
+++ AddressBook.java
@@ -0,0 +1,12 @@
+package com.blbz;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+	}
+
+}
