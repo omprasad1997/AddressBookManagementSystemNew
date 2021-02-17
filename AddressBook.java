793119473c1cd721c718908ddcabcaab27f3ab52
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-18 00:19:57 +0530

    Create Contact List

--- AddressBook.java
+++ AddressBook.java
@@ -1,9 +1,22 @@
 package com.blbz;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome");
-	}
+		Scanner sc = new Scanner(System.in);
 
+		Contact dMart = new Contact("Pushpak","Ghatode","Kailash Nagar","Pune","Maharashtra","400001","384792876","abc@gmail.com");
+		ArrayList<Contact> contact = new ArrayList<Contact>();
+		contact.add(dMart);
+
+		System.out.println("-----------------------------------------------------------Welcome to Address Book Manangement System--------------------------------------------------------\n");
+
+		System.out.printf("%-20s%-20s%-20s%-20s%-20s%-20s%-20s%-20s%n","First Name","Last Name","Address","City","State","Zip","Phone Number","E-mail");
+		for(int i=0;i<contact.size();i++){
+			contact.get(i).showDetails();
+		}
+	}
 }
