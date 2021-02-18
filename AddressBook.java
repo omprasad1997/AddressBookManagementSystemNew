3e81e780dfa21f10c2dd43695e746ee5bc49a4f8
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-18 07:50:35 +0530

    Edit Existing Contact

--- AddressBook.java
+++ AddressBook.java
@@ -14,27 +14,48 @@ public class AddressBook {
 
 		System.out.println("-----------------------------------------------------------Welcome to Address Book Manangement System--------------------------------------------------------\n");
 
-		ArrayList<String> details = new ArrayList<String>();
-		System.out.println("First Name : ");
-		details.add(sc.next());
-		System.out.println("Last Name : ");
-		details.add(sc.next());
-		System.out.println("Address : ");
-		details.add(sc.next());
-		System.out.println("City Name : ");
-		details.add(sc.next());
-		System.out.println("State Name : ");
-		details.add(sc.next());
-		System.out.println("Zip code : ");
-		details.add(sc.next());
-		System.out.println("Phone Number: ");
-		details.add(sc.next());
-		System.out.println("E-mail id : ");
-		details.add(sc.next());
-		Contact input=new Contact(details.get(0),details.get(1),details.get(2),details.get(3),details.get(4),details.get(5),details.get(6),details.get(7));
-		contact.add(input);
-		System.out.println("You have successfully added new contact.");
-		System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------");					
+		System.out.printf("%-20s%-20s%-20s%-20s%-20s%-20s%-20s%-20s%n","First Name","Last Name","Address","City","State","Zip","Phone Number","E-mail");
+		for(int i=0;i<contact.size();i++){
+			contact.get(i).showDetails();
+		}
+		System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------");	
+	
+		
+		int index=0;
+		boolean isPresent=false;
+		System.out.print("Enter person name do you want to edit : ");
+		String tempFirstName=sc.next();
+		for(int i = 0; i < contact.size(); i++) {
+			if(contact.get(i).getFirstName().equals(tempFirstName)) {
+				isPresent=true;
+				index=i;
+				break;		
+			}
+		}
+
+		if(isPresent) {
+			System.out.println("First Name : ");
+			contact.get(index).setFirstName(sc.next());
+			System.out.println("Last Name : ");
+			contact.get(index).setLastName(sc.next());
+			System.out.println("Address : ");
+			contact.get(index).setAddress(sc.next());
+			System.out.println("City Name : ");
+			contact.get(index).setCity(sc.next());
+			System.out.println("State Name : ");
+			contact.get(index).setState(sc.next());
+			System.out.println("Zip code : ");
+			contact.get(index).setZip(sc.next());
+			System.out.println("Phone Number: ");
+			contact.get(index).setPhoneNumber(sc.next());
+			System.out.println("E-mail id : ");
+			contact.get(index).setEmail(sc.next());
+			System.out.println("You have successfully edited contact.");
+		}
+		else {
+			System.out.println("Invalid Name");
+		}	
+		System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------");
 	}
 }
 
